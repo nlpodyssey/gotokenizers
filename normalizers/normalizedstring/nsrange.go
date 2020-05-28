dc5146f46aea475e23725f9e3f158df05362f17d
Marco Nicola <marconicola@disroot.org>
2020-05-28 20:03:06 +0200

    Rename packages without using underscores
    following a more common naming convention

--- normalizers/normalizedstring/nsrange.go
+++ normalizers/normalizedstring/nsrange.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package normalized_string
+package normalizedstring
 
 // NSRange is implemented by values which represent a range usable by the
 // NormalizedString to index its content.
--- normalizers/normalizers.go
+++ normalizers/normalizers.go
@@ -4,10 +4,10 @@
 
 package normalizers
 
-import "github.com/nlpodyssey/gotokenizers/normalizers/normalized_string"
+import "github.com/nlpodyssey/gotokenizers/normalizers/normalizedstring"
 
 // Normalizer is implemented by any value that has a Normalize method,
 // which takes care of pre-processing strings.
 type Normalizer interface {
-	Normalize(ns *normalized_string.NormalizedString) error
+	Normalize(ns *normalizedstring.NormalizedString) error
 }
